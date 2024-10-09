939c7ff949dff6d1ae25de251d04c4a2ede79e69
Xrash <natnaelbelayteklemariam@gmail.com>
2024-10-09 11:00:48 +0300

    created the entity

---
+++ blog/lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,17 @@
+import '../../domian/entity/user.dart';
+
+class UserModel extends User {
+  UserModel({
+    required super.name,
+    required super.email,
+    required super.id,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> map) {
+    return UserModel(
+      name: map["name"],
+      email: map["email"],
+      id: map["id"],
+    );
+  }
+}
---
+++ blog/lib/features/auth/domian/entity/user.dart
@@ -0,0 +1,7 @@
+class User{
+	final String email;
+	final String name;
+	final String id;
+
+	const User({required this.name, required this.email, required this.id});
+}
